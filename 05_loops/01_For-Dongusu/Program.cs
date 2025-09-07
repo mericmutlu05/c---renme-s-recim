9bb201fc5e29436b002dd2a428d614944267b67a
Meriç Beytullah Mutlu <mericmutlu23@gmail.com>
2025-09-07 03:30:07 +0300

    .

---
+++ 05_loops/01_For-Dongusu/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace _01_For_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Artarak Dönen For Döngüsü
+            //for(int sayi = 0; sayi <= 10; sayi++)
+            // {
+            //     Console.WriteLine("Merhaba");
+            //     Console.WriteLine(sayi);
+            // }
+            #endregion
+
+            #region Artarak Dönen For Döngüsü(100 ile 200 arası)
+            //for(int i = 100; i < 201; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region azalarak dönen for döngüsü
+
+            //for(int sayi=10;sayi>=0;sayi--)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+
+
+            #endregion
+
+            #region karakterler arası dönen for döngüsü
+
+            //for (char i = 'A'; i < 'Z'; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region Döngü İçerisinde continue kullanımı
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    if (i==5)
+            //    continue;
+            //  Console.WriteLine(i);
+            //}
+
+
+            #endregion
+
+            #region Döngü içerisinde break kullanımı
+
+            //for (int i = 0; i <=10; i++)
+            //{
+            //    if (i==3)
+
+            //        break;
+            //        Console.WriteLine(i);
+
+            //}
+
+            #endregion
+
+            #region 5'er 5'er artan for döngüsü
+
+            //for (int i = 0; i <=100; i+=5)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region sonsuz for döngüsü
+            //for (; ; )
+            //{
+            //    Console.WriteLine("merhaba");
+            //}
+            #endregion
+
+            #region iç içe for döngüsü
+
+            //for (int i = 0; i <=10; i++)
+            //{
+            //    for (int j = 0; j <=10; j++)
+            //    {
+            //        Console.WriteLine("i:{0}j:{1}",i,j);
+            //    }
+            //}
+
+
+            #endregion
+
+        }
+    }
+}
---
+++ 05_loops/02_For-Dongusu/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _02_For_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 ile 50 arasındaki çift sayıları ekrana yazdıran program.
+
+            //for (int i = 0; i <=50; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            #endregion
+
+            #region çarpım tablosu iç içe for döngüsü
+
+            //for (int i = 0; i <=10; i++)
+            //{
+            //    for (int s = 1; s<=10; s++)
+            //    {
+            //        int carpım = i * s;
+            //        Console.WriteLine($"i:{i}* s:{s}= {carpım}");
+            //    }
+            //}
+
+
+            #endregion
+
+            #region alfabe örneği
+            //string alfabe = "!*.-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            //Random ran = new Random();
+            //for (int i = 0; i <8; i++)
+            //{
+            //    Console.WriteLine(alfabe[ran.Next(0,alfabe.Length)]);
+            //}
+            #endregion
+
+            #region ornek
+            //klavyeden girilen bir cümlenin her harfini döngü ile alt alta yazdıran programı yazalım
+
+            //Console.WriteLine("bir kelime giriniz");
+            //string kelime = Convert.ToString(Console.ReadLine());
+
+            //for (int i=0 ; i < kelime.Length; i++)
+            //{
+            //    Console.WriteLine(kelime[i]);
+            //}
+
+            #endregion
+        }
+    }
+}
