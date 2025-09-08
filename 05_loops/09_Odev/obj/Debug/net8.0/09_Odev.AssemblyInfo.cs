37bc224c7866f2d3526f3b09b74bd56b2d600a1f
Meriç Beytullah Mutlu <mericmutlu23@gmail.com>
2025-09-08 20:26:26 +0300

    .

--- 05_loops/09_Odev/obj/Debug/net8.0/09_Odev.AssemblyInfo.cs
+++ 05_loops/09_Odev/obj/Debug/net8.0/09_Odev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("09_Odev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cef1aa3b66fc758e746df72fc614f8e8a8c2d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("09_Odev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("09_Odev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 06_Arrays/06_Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Array Nedir?
+
+            /*
+            Diziler (Arrays) 
+            string ifadeler aslında birer dizidir.
+            diziler birden çok yapıyı içinde barındıran collection yapılardır.
+            dizileri tanımlarken sayısını belirttiğimiz veya belirtmediğimiz durumlar vardır.
+            dizinin elemanlarını index numaralarına göre işleme alırız ve index numarası her zaman 0'dan başlar.
+            dizi tanımlama => int[] dizi=new int[] => instance / örneklem
+             */
+
+
+            #endregion
+
+            #region Array Tanımlaması
+
+            int[] sayilar = {10,2,5,6,89,56,1 };
+            //Console.WriteLine(sayilar);
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(i + ". eleman =" + sayilar[i]);
+            }
+
+
+            #endregion
+
+        }
+    }
+}
